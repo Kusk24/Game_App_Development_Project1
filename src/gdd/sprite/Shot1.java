f0e001e5d2ed5dc2fd13a510fef812a8318cc70b
Kusk24 <winyumg2003a@gmail.com>
2025-07-22 06:11:35 +0700

    feat: Vertical Shot Animation Added

---
+++ src/gdd/sprite/Shot1.java
@@ -0,0 +1,53 @@
+package gdd.sprite;
+
+import static gdd.Global.*;
+import javax.swing.ImageIcon;
+import java.awt.*;
+
+public class Shot1 extends Sprite {
+
+    private static final int H_SPACE = 20;
+    private static final int V_SPACE = 1;
+
+
+    public Shot1() {
+    }
+
+    @Override
+    public void act() {
+
+    }
+
+    public Shot1(int x, int y, int level) {
+
+        initShot(x, y, level);
+    }
+
+    private void initShot(int x, int y, int level) {
+
+        var ii = shotLevel(level);
+
+        // Scale the image to use the global scaling factor
+        var scaledImage = ii.getImage().getScaledInstance(ii.getIconWidth() * SCALE_FACTOR,
+                ii.getIconHeight() * SCALE_FACTOR,
+                java.awt.Image.SCALE_SMOOTH);
+        setImage(scaledImage);
+
+        setX(x + H_SPACE);
+        setY(y - V_SPACE);
+    }
+
+    public ImageIcon shotLevel(int level){
+        if (level == 1) {
+            return new ImageIcon(IMG_SHOT);
+        } else if (level == 2) {
+            return new ImageIcon(IMG_SHOT2);
+        } else if (level == 3) {
+            return new ImageIcon(IMG_SHOT3);
+        } else if (level == 4) {
+            return new ImageIcon(IMG_SHOT4);
+        } else {
+            return new ImageIcon(IMG_SHOT);
+        }
+    }
+}
